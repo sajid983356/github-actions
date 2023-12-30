06362100bb766acbf39c1f8665b5989f7a04567b
sshaikh <sajid.shaikh@finicity.com>
2023-12-30 11:56:58 +0530

    r

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          mv build Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
